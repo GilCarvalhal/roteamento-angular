976e5064f1b8b1b1f404cf0aadb0e90143c2ccca
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-01 00:48:18 -0300

    Angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+
+const routes: Routes = [
+  { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PrimeiraPaginaComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
