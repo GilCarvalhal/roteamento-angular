ab3bd96b5550d2a67f27b04987793349e8f90884
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-04 13:52:57 -0300

    navegacao entre paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
 const routes: Routes = [
   { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
+  { path: 'segunda-pagina', component: SegundaPaginaComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, PrimeiraPaginaComponent],
+  declarations: [AppComponent, PrimeiraPaginaComponent, SegundaPaginaComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [],
