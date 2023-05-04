47d8690543d423c124253033684d865f89e87352
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-04 15:42:50 -0300

    redirecionamento de url & page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
-
+const routes: Routes = [
+  { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
+  { path: 'segunda-pagina', component: SegundaPaginaComponent },
+  { path: '', redirectTo: 'primeira-pagina', pathMatch: 'full' },
+  {path: '**', component: PaginaNaoEncontradaComponent}
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
-const routes: Routes = [
-  { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
-  { path: 'segunda-pagina', component: SegundaPaginaComponent },
-];
-
 @NgModule({
-  declarations: [AppComponent, PrimeiraPaginaComponent, SegundaPaginaComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    PrimeiraPaginaComponent,
+    SegundaPaginaComponent,
+    PaginaNaoEncontradaComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
